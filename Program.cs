9dedc548d82910bb97263aca79af8cf54908396c
Дмитрий Хромов <Khr.Dmitriy@gmail.com>
2022-06-21 23:00:33 +0300

    Added code for solving the problem using two methods

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[]arr = {"hello", "2","word",";-)"};
+
+string[] ChangeArrayToTreeSimbols(string[] arr)
+{
+    int j = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i].Length <= 3)
+        {
+            arr[j] = arr[i];
+            j++;
+        }
+    }
+    string[] newArr = new string[j];
+    for(int i = 0; i < j; i++)
+    {
+        newArr[i] = arr[i];
+    }
+    return newArr;
+}
+
+
+void PrintArray(string[] newArr)
+{
+    for(int i = 0; i < newArr.Length; i++)
+    {
+        Console.Write(newArr[i] + "  ");
+    }
+}
+
+PrintArray(arr);
+Console.WriteLine();
+PrintArray(ChangeArrayToTreeSimbols(arr));
